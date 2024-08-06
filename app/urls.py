f77197a436f1c97a7fd63e4dc723f5b8882598ae
Anh-Us <lenhaanh1004@gmail.com>
2024-08-07 00:57:30 +0700

    7/8

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import updateItem
+
+
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+   # path('update_item/', views.updateItem, name="updateItem"),
+    path('update_item/', updateItem, name='update_item'),
+
 ]
